34bb6fbc6fb91d06cda82be564c3f63f7220a502
Jeff Anderson <jeff.anderson@digecor.com>
2015-09-15 00:48:20 -0600

    Add some example files
    From the GStream Application Developer's Manual.

---
+++ main3.cpp
@@ -0,0 +1,20 @@
+#include <gst/gst.h>
+
+int main(int argc, char* argv[]) {
+	GstElementFactory* factory;
+	GstElement* element;
+	gst_init(&argc, &argv);
+	factory = gst_element_factory_find("fakesrc");
+	if (!factory) {
+		g_print("Failed to find factory of type ’fakesrc’\n");
+		return -1;
+	}
+	element = gst_element_factory_create(factory, "source");
+	if (!element) {
+		g_print("Failed to create element, even though its factory exists!\n");
+		return -1;
+	}
+	gst_object_unref(GST_OBJECT(element));
+	return 0;
+}
+
